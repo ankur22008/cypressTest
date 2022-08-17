5b899082bb60701b2b33e0cef2332c78bc03799c
Ankur Jindal <ankur22008@gmail.com>
2022-08-17 17:14:15 +1000

    Update festivals_api.js

--- cypress/e2e/user/festivals_api.js
+++ cypress/e2e/user/festivals_api.js
@@ -17,16 +17,15 @@ describe('Energy Australia Test', () => {
     it('Band name returned by API should be displayed in sorted manner in UI', () => {
         let arr = [];
         cy.get('ol li').each(($btn, index, $list) => {
-            arr.push($btn.text())
             if (index % 2 === 0) {
                 cy.log($btn.text())
+                arr.push($btn.text())
             }
             else {
                 //do nothing
             }
         }).then(() => {
             cy.wrap(arr).should("equal", arr.sort());
-            cy.log('Test Success')
 
         })
     })
