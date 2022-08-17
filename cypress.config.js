0dc016550e4ea0b7f68c3bd3ec5fb66e29c6c571
Ankur Jindal <ankur22008@gmail.com>
2022-08-17 17:10:14 +1000

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require('cypress');
+
+
+
+// Populate process.env with values from .env file
+
+
+module.exports = defineConfig({
+  env: {
+
+    baseUrl: 'http://localhost:4200/festivals'
+  },
+
+  chromeWebSecurity: false,
+  retries: { runMode: 2, openMode: 1 },
+  screenshotOnRunFailure: true,
+  video: false,
+  //experimentalSessionAndOrigin: true,
+
+
+  defaultCommandTimeout: 20000,
+
+
+
+  viewportWidth: 1000,
+  viewportHeight: 600,
+
+  e2e: {
+
+
+    specPattern:
+      ["cypress/e2e/user/festivals_api.js",
+      ],
+
+    supportFile: 'cypress/support/index.js'
+  },
+});
+
+
